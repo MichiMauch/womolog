40e43b586e3fd4307a83079581f82afe1094cf68
MichiMauch <michi.mauch@gmail.com>
2024-02-18 19:02:02 +0100

    es gibt news

---
+++ push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Frage den Benutzer, ob das Python-Skript ausgeführt werden soll
+read -p "Möchtest du 'python3 generate_stats.py' ausführen? (j/n) " answer
+
+# Überprüfe die Benutzereingabe
+if [[ $answer = j ]]; then
+    # Führe das Python-Skript aus
+    python3 generate_stats.py
+
+    # Überprüfe, ob das Python-Skript erfolgreich war
+    if [ $? -ne 0 ]; then
+        echo "Das Python-Skript ist fehlgeschlagen. Skript wird beendet."
+        exit 1
+    fi
+fi
+
+# Füge alle Änderungen hinzu, erstelle einen Commit und pushe
+git add .
+git commit -m "es gibt news"
+git push origin main
