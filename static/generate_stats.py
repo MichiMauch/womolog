a8844024444466010e71991b23a5005265b1840b
MichiMauch <michi.mauch@gmail.com>
2024-02-20 19:57:36 +0100

    neuer test

--- static/generate_stats.py
+++ static/generate_stats.py
@@ -3,7 +3,9 @@ import requests
 import frontmatter
 import json
 from collections import defaultdict
+from tqdm import tqdm
 from time import sleep
+
 
 def get_location_data(lat, lon):
     try:
@@ -28,44 +30,49 @@ def get_location_data(lat, lon):
         print(f"Fehler beim Abrufen der Daten für {lat}, {lon}: {e}")
         return None, None
 
-def main():
-    content_dir = 'content/plaetze'
-    stats = defaultdict(lambda: {"Besuche": 0, "Orte": [], "country_code": ""})
-
-    print(f"Starte Verarbeitung im Verzeichnis: {content_dir}")
+def get_md_files(content_dir):
+    md_files = []
     for root, dirs, files in os.walk(content_dir):
         for file in files:
             if file.endswith('.md'):
                 path = os.path.join(root, file)
-                post = frontmatter.load(path)
+                md_files.append(path)
+    return md_files
 
-                if 'coordinates' in post.metadata:
-                    lat = post.metadata['coordinates']['latitude']
-                    lon = post.metadata['coordinates']['longitude']
 
-                    country, location_info = get_location_data(lat, lon)
+def main():
+    content_dir = 'content/plaetze'
+    md_files = get_md_files(content_dir)  # Sammeln aller MD-Dateien
+    stats = defaultdict(lambda: {"Besuche": 0, "Orte": [], "country_code": ""})
 
-                    if country:
-                        stats[country]["Besuche"] += 1
-                        stats[country]["Orte"].append({
-                            "Ort": location_info['tourism'],
-                            "Strasse": location_info['road'],
-                            "Stadt": location_info['town'],
-                            "PLZ": location_info['postcode'],
-                            "Land": location_info['country'],
-                            "Dateiname": file
-                        })
-                        # Setze den Ländercode, falls noch nicht gesetzt
-                        if stats[country]["country_code"] == "":
-                            stats[country]["country_code"] = location_info['country_code']
+    print(f"Starte Verarbeitung im Verzeichnis: {content_dir}")
+    # Verwende tqdm über die gesamte Liste der MD-Dateien
+    for path in tqdm(md_files, desc="Verarbeite Dateien"):
+        post = frontmatter.load(path)
+        file = os.path.basename(path)
 
-                        sleep(1)
+        if 'coordinates' in post.metadata:
+            lat = post.metadata['coordinates']['latitude']
+            lon = post.metadata['coordinates']['longitude']
+            country, location_info = get_location_data(lat, lon)
+
+            if country:
+                stats[country]["Besuche"] += 1
+                stats[country]["Orte"].append({
+                    "Ort": location_info['tourism'],
+                    "Strasse": location_info['road'],
+                    "Stadt": location_info['town'],
+                    "PLZ": location_info['postcode'],
+                    "Land": location_info['country'],
+                    "Dateiname": file
+                })
+                if stats[country]["country_code"] == "":
+                    stats[country]["country_code"] = location_info['country_code']
 
     with open('data/stats.json', 'w') as f:
         json.dump(stats, f, ensure_ascii=False, indent=4)
 
     print("Statistiken erfolgreich erstellt.")
-
 
 if __name__ == "__main__":
     main()
